bcc45902a9b450411bdd00e2c7611b30aa205db5
yazidalg <yazid.al2418@gmail.com>
2024-04-29 07:34:01 +0700

    initial commit

---
+++ JurnalMod10_1302223047/Controllers/Mahasiswa.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace JurnalMod10_1302223047.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Mahasiswa : ControllerBase
+    {
+        private static List<ModelMahasiswa> listOfMahasiswa = new List<ModelMahasiswa>
+        {
+            new ModelMahasiswa("Al", "1302223047", 2022, ["IMK", "PBO"]),
+        };
+
+        [HttpGet]
+        public IEnumerable<ModelMahasiswa> Get()
+        {
+            return listOfMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public ModelMahasiswa Get(int id)
+        {
+            return listOfMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ModelMahasiswa mahasiswa)
+        {
+            listOfMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ModelMahasiswa mahasiswa)
+        {
+            listOfMahasiswa[id] = mahasiswa;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listOfMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ JurnalMod10_1302223047/Controllers/ModelMahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace JurnalMod10_1302223047.Controllers
+{
+    public class ModelMahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public int year { get; set; }
+        public List<string> courses { get; set; }
+
+        public ModelMahasiswa(string nama, string nim, int year, List<string> courses) 
+        { 
+            this.nama = nama;
+            this.nim = nim;
+            this.year = year;
+            this.courses = courses;
+        }
+    }
+}
